d1870160fc344b4563f7fca2fdbae19532680576
Test User <test@example.com>
2026-03-05 15:32:17 +0400

    feat(skeletons): create skeleton components index file

---
+++ apps/frontend/src/renderer/components/skeletons/index.ts
@@ -0,0 +1,7 @@
+export { ChangelogSkeleton } from './ChangelogSkeleton';
+export { IssueListSkeleton } from './IssueListSkeleton';
+export { PRListSkeleton } from './PRListSkeleton';
+export { ProjectListSkeleton } from './ProjectListSkeleton';
+export { TaskCardSkeleton } from './TaskCardSkeleton';
+export { SpecDetailSkeleton } from './SpecDetailSkeleton';
+export { PatternSuggestionsSkeleton } from './PatternSuggestionsSkeleton';
